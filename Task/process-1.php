61db9d90c44ceaca99b3eff811a8324a3a2d3b37
Anbu0001 <144221382+Anbu0001@users.noreply.github.com>
2024-10-25 12:54:58 +0530

    Add files via upload

---
+++ Task/process-1.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Done</h1>
+
+
+<?php  
+ include("dbcon.php");
+
+ $getdata = isset($_GET['id']);
+ $getdata = $_GET['id'];
+
+
+if($getdata){
+    $process1 = mysqli_query($conn,"UPDATE employee_table SET process_stage_1=0 ,  process_stage_2=1 where id=$getdata");
+  
+    echo "<br> $process1 ";
+
+    // header ("Location:index.php");
+
+
+    header ("Location:process-2.php");
+
+
+
+} else {
+    echo "GET Data Error!!!!!!";
+}
+
+?>
+<!-- \\\\\\\\\\\\\\\\\\\\\\\\=-----------process2-----------------\\\\\\\\\\\\\\\\\\\\\\\\-->
+<?php  
+ include("dbcon.php");
+
+ $getdata1 = isset($_GET['process2']);
+ $getdata1 = $_GET['process2'];
+
+
+if($getdata1){
+    $process2 = mysqli_query($conn,"UPDATE employee_table SET process_stage_2=0 ,process_stage_3=1  where id=$getdata1");
+  
+    echo "<br> $process2 ";
+
+    // header ("Location:index.php");
+
+
+    header ("Location:process-3.php");
+
+
+
+} else {
+    echo "GET Data Error!!!!!!";
+}
+
+?>
+
+<!-- \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\-----------process3---------\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\-->
+<?php  
+ include("dbcon.php");
+
+ $getdata2 = isset($_GET['process3']);
+ $getdata2 = $_GET['process3'];
+
+
+if($getdata2){
+    $process3 = mysqli_query($conn,"UPDATE employee_table SET process_stage_3=0 , process_stage_1=1  where id=$getdata2");
+  
+    echo "<br> $process3 ";
+
+    header ("Location:index.php");
+
+
+
+
+
+} else {
+    echo "GET Data Error!!!!!!";
+}
+
+?>
---
+++ Task/process-2.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php 
+
+include("dbcon.php");
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST['name'];
+        $age = $_POST['age'];
+        $process1 = $_POST['process1'];
+        $process2 = $_POST['process2'];
+        $process3 = $_POST['process3'];
+        $main_status=1;
+
+        $sqlstatement = mysqli_query($conn,"INSERT INTO employee_table (
+        username,
+        userage,
+         process_stage_1,
+         process_stage_2,	
+         process_stage_3,		
+         main_status
+         ) 
+         VALUES 
+         (
+         '$name',
+          $age,
+         '$process1',
+         '$process2',
+         '$process3',
+          $main_status
+         )"
+        );
+        
+
+        
+
+        if ($sqlstatement) {
+            echo "<p>SuccessFull!</p> ";      
+        
+        } else {
+            
+            echo "<p>Error: " . $conn->error . "</p> ";
+        }
+    }
+
+?>
+
+
+
+<?php  
+    $i=1;
+    $process1 = mysqli_query($conn,"SELECT * FROM employee_table where process_stage_1=0 && process_stage_2=1");
+    while($getdata=mysqli_fetch_array($process1)){
+ 
+
+   ?>
+   <table >
+    <tr >
+        <th>s.no</th>
+        <th>Name</th>
+        <th>Age</th>
+    </tr>
+    <tr>
+    <td>
+            <?php echo $i?>
+        </td>
+        <td>
+            <?php echo $getdata["username"] ?>
+        </td>
+        <td>
+            <?php echo $getdata["userage"] ?>
+            </td>
+
+            <td>
+                <a href="process-1.php?process2=<?php echo $getdata['id']; ?>">Move</a>
+            </td>
+
+ 
+        
+        
+        <?php $i++; }
+
+?>
+       
+        <?php 
+   " </tr>
+   </table>"
+
+   ?>
+
+
+
+
+
